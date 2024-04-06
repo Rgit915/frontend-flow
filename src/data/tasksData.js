c83b6661cd00c96ce23bdb837481d39304bb515c
Rgit915 <rora.goitom@gmail.com>
2024-04-05 21:36:32 -0400

    Add sample tasksData as array of objects

---
+++ src/data/tasksData.js
@@ -0,0 +1,23 @@
+import taskImage1 from '../assets/images/pc-icon.png';
+
+const taskData = [
+  {
+    id: '001',
+    image: taskImage1,
+    content: 'Task #001',
+  },
+  {
+    id: '002',
+    image: taskImage1,
+    content: 'Task #002',
+  },
+  {
+    id: '003',
+    image: taskImage1,
+    content: 'Task #003',
+  }
+
+
+];
+
+export default taskData;
