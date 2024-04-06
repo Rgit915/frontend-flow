a68807a4c6ffc991dabdf8e04d105e7afe20c32f
Rgit915 <rora.goitom@gmail.com>
2024-04-05 21:49:26 -0400

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+    <div className="text-center text-sm mt-4 text-indigo-950 py-8 font-bold">
+      Coded by{" "}
+      <a
+        href="https://www.linkedin.com/in/rora-alem"
+        className=" underline hover:text-indigo-500"
+      >
+        Rora Alem
+      </a>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer;
