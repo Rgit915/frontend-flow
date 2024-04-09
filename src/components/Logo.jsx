e1df5bbd68bde547113cc779b26f994b54688032
Rgit915 <rora.goitom@gmail.com>
2024-04-09 07:09:23 -0400

    Modify logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,9 +4,9 @@ import logo from '../assets/images/pc-icon.png';
 const Logo = () => {
   return (
     <div className='Logo h-8 w-8'>
-       <a href="#" className="flex items-center space-x-2">
+       <a href="/" className="flex items-center space-x-2">
       <img src={logo} className="cursor-pointer rounded-full" alt="logo" />
-      <span className="text-xl font-semibold font-great-vibes">Frontend Flow</span>
+      <span className="font-semibold font-great-vibes">FrontendFLow</span>
     </a>
     </div>
   )
