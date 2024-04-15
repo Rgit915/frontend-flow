5b0c8f2555fcd0e72039f7ac9da64373c629e455
Rgit915 <rora.goitom@gmail.com>
2024-04-15 13:07:45 -0400

    Update tasks sample data

--- src/data/tasksData.js
+++ src/data/tasksData.js
@@ -1,21 +1,32 @@
-import taskImage1 from '../assets/images/pc-icon.png';
-
+import taskImage1 from '../assets/images/task-001.png';
+import taskImage2 from '../assets/images/task-002.png';
+import taskImage3 from '../assets/images/task-003.png';
+import taskImage4 from '../assets/images/task-004.png';
 const taskData = [
   {
     id: '001',
     image: taskImage1,
-    content: 'Task #001',
+    title: 'Result summary component',
+    description: ['HTML', 'CSS', 'JS'],
   },
   {
     id: '002',
-    image: taskImage1,
-    content: 'Task #002',
+    image: taskImage2,
+    title: 'Newsletter signUp page',
+    description: ['REACT', 'TAILWINDCSS', 'FORMIK'],
   },
   {
     id: '003',
-    image: taskImage1,
-    content: 'Task #003',
-  }
+    image: taskImage3,
+    title: 'Pod request access landing page',
+    description: ['REACT','TAILWINDCSS' ,'FORMIK'],
+  },
+  {
+    id:'004',
+    image: taskImage4,
+    title:'Space tourism multi-page website',
+    description: ['REACT', 'TAILWINDCSS'],
+  },
 
 
 ];
