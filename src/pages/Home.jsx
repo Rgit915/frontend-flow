1df70e66cfa2b4595ee622e5cdc32cf5799e3b33
Rgit915 <rora.goitom@gmail.com>
2024-04-15 13:10:41 -0400

    Implement overlay task content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
-import tasksData from '../data/tasksData';
+import React from "react";
+import tasksData from "../data/tasksData";
+import Button from "../components/Button";
 
 const Home = () => {
   return (
     <main>
       <div className="grid grid-cols-2 gap-10 p-5 min-[400px]:grid-cols-3">
         {tasksData.map((task) => (
-          <div key={task.id} className="w-full aspect-video shadow-[0_0_10px_indigo] relative">
-            <span className="absolute p-2 bg-indigo-950 text-white rounded-br-lg">
+          <div
+            key={task.id}
+            className="w-full aspect-video shadow-[0_0_10px_indigo] relative rounded-xl"
+          >
+            {/* <span className="absolute p-2 bg-indigo-950 text-white rounded-br-lg">
             <p>Task# {task.id}</p>
-            </span>
-            <img src={task.image} alt={`PC Icon ${task.id}`}  />
-            <p>{task.content}</p>
+            </span> */}
+            <img
+              className="bg-cover rounded-xl hover:scale-105 transition-transform duration-700 ease-in-out"
+              src={task.image}
+              alt={`PC Icon ${task.id}`}
+            />
+            <div className="overlay rounded-b-xl space-y-2 font-mono">
+              <ul className="list-none flex space-x-2  ">
+                {/* Using map to render list items */}
+                {task.description.map((item, index) => (
+                  <li key={index} className=" text-slate-200 font-bold  px-2">
+                    {item}
+                  </li>
+                ))}
+              </ul>
+              <div className="flex space-x-2">
+                <Button value={"Live Demo"}/>
+                <p>|</p>
+                <Button value={"Code Repo"}/>
+                <p>|</p>
+                <Button value={"Watch here"}/>
+              </div>
+            </div>
           </div>
         ))}
       </div>
     </main>
   );
-}
+};
 
 export default Home;
