fdfb921b1283248effc9ec4bbd779a61b1030963
Rgit915 <rora.goitom@gmail.com>
2024-04-18 20:33:31 -0400

    tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'great-vibes': ['Great Vibes', 'cursive'],
+        'roboto': ['roboto', 'sans'],
+      },
+    },
+
+  },
+  plugins: [],
+}
+
