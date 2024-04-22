1f8c6627e9b5d514fa93a18151c7d53c6bc75186
Rgit915 <rora.goitom@gmail.com>
2024-04-21 23:49:36 -0400

    Add Link component from react router

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Button = (props) => {
   return (
     <div>
-      <button className={`text-slate-700 font-medium px-2 cursor-pointer rounded-3xl bg-white hover:bg-black hover:text-white`}
-      onClick={props.onClick}>{props.value}
-      </button>
+      <Link className={`text-slate-700 font-medium px-2 cursor-pointer rounded-3xl bg-white hover:bg-black hover:text-white`}
+      onClick={props.onClick} to={props.to}>{props.value}
+      </Link>
     </div>
   );
 };
