074fc1cc170d3fbe747778e716d373dd4975b762
Rgit915 <rora.goitom@gmail.com>
2024-04-22 14:25:38 -0400

    Add sample data

--- src/data/tasksData.js
+++ src/data/tasksData.js
@@ -1,34 +1,63 @@
-import taskImage1 from '../assets/images/task-001.png';
-import taskImage2 from '../assets/images/task-002.png';
-import taskImage3 from '../assets/images/task-003.png';
-import taskImage4 from '../assets/images/task-004.png';
+import taskImage1 from '../assets/images/task001.png';
+import taskImage2 from '../assets/images/task002.png';
+import taskImage3 from '../assets/images/task003.png';
+import taskImage4 from '../assets/images/pc-icon.png';
+import taskImage5 from '../assets/images/pc-icon.png';
+import taskImage6 from '../assets/images/pc-icon.png';
 const taskData = [
   {
     id: '001',
     image: taskImage1,
     title: 'Result summary component',
     description: ['HTML', 'CSS', 'JS'],
+    codeRepo:"https://github.com/Rgit915/results-summary-component",
+    liveLink:"https://rgit915.github.io/results-summary-component/",
+    watchHere:"",
   },
   {
     id: '002',
     image: taskImage2,
     title: 'Newsletter signUp page',
     description: ['REACT', 'TAILWINDCSS', 'FORMIK'],
+    codeRepo:"https://github.com/Rgit915/newsletter-sign-up-with-success-message",
+    liveLink:"https://newsletter-signup-rora-alem.netlify.app/",
+    watchHere:"",
   },
   {
     id: '003',
     image: taskImage3,
     title: 'Pod request access landing page',
-    description: ['REACT','TAILWINDCSS' ,'FORMIK'],
+    description: ['REACT', 'TAILWINDCSS', 'FORMIK'],
+    codeRepo:"https://github.com/Rgit915/pod-request-access-landing-page",
+    liveLink:"https://pod-request-access-landing-page-rora.netlify.app/",
+    watchHere:"",
   },
   {
-    id:'004',
+    id: '004',
     image: taskImage4,
-    title:'Space tourism multi-page website',
+    title: 'Space tourism multi-page website',
     description: ['REACT', 'TAILWINDCSS'],
+    codeRepo:"",
+    liveLink:"",
+    watchHere:"",
   },
 
-
+  {
+    id: '004',
+    image: taskImage5,
+    title: '',
+    description: [''],
+    codeRepo:"",
+    liveLink:"",
+  },
+  {
+    id: '004',
+    image: taskImage6,
+    title: '',
+    description: [''],
+    codeRepo:"",
+    liveLink:"",
+  },
 ];
 
 export default taskData;
