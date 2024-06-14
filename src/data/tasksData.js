34a984aaae57ea8a808e2c40b5c5ef3e4bce9c09
Rgit915 <rora.goitom@gmail.com>
2024-06-14 11:39:19 -0400

    Update tasks data

--- src/data/tasksData.js
+++ src/data/tasksData.js
@@ -2,14 +2,18 @@ import taskImage1 from '../assets/images/task001.png';
 import taskImage2 from '../assets/images/task002.png';
 import taskImage3 from '../assets/images/task003.png';
 import taskImage4 from '../assets/images/pc-icon.png';
-import taskImage5 from '../assets/images/pc-icon.png';
-import taskImage6 from '../assets/images/pc-icon.png';
+import taskImage5 from '../assets/images/task005.png';
+import taskImage6 from '../assets/images/task006.png';
+import taskImage7 from '../assets/images/task007.png';
+import taskImage8 from '../assets/images/task008.png';
+
 const taskData = [
   {
     id: '001',
     image: taskImage1,
     title: 'Result summary component',
-    description: ['HTML', 'CSS', 'JS'],
+    description:'',
+    tools: ['HTML', 'CSS', 'JS'],
     codeRepo:"https://github.com/Rgit915/results-summary-component",
     liveLink:"https://rgit915.github.io/results-summary-component/",
     watchHere:"",
@@ -18,7 +22,8 @@ const taskData = [
     id: '002',
     image: taskImage2,
     title: 'Newsletter signUp page',
-    description: ['REACT', 'TAILWINDCSS', 'FORMIK'],
+    description:'This challenge is to built signup page',
+    tools: ['REACT', 'TAILWINDCSS', 'FORMIK'],
     codeRepo:"https://github.com/Rgit915/newsletter-sign-up-with-success-message",
     liveLink:"https://newsletter-signup-rora-alem.netlify.app/",
     watchHere:"",
@@ -27,7 +32,8 @@ const taskData = [
     id: '003',
     image: taskImage3,
     title: 'Pod request access landing page',
-    description: ['REACT', 'TAILWINDCSS', 'FORMIK'],
+    description:'',
+    tools: ['REACT', 'TAILWINDCSS', 'FORMIK'],
     codeRepo:"https://github.com/Rgit915/pod-request-access-landing-page",
     liveLink:"https://pod-request-access-landing-page-rora.netlify.app/",
     watchHere:"",
@@ -36,27 +42,48 @@ const taskData = [
     id: '004',
     image: taskImage4,
     title: 'Space tourism multi-page website',
-    description: ['REACT', 'TAILWINDCSS'],
-    codeRepo:"",
+    description:'',
+    tools: ['REACT', 'TAILWINDCSS'],
+    codeRepo:"https://github.com/Rgit915/space-tourism-website",
     liveLink:"",
     watchHere:"",
   },
 
   {
-    id: '004',
+    id: '005',
     image: taskImage5,
-    title: '',
-    description: [''],
-    codeRepo:"",
-    liveLink:"",
+    title: 'Base Apparel Coming Soon Page',
+    description:'',
+    tools: ['REACT', 'TAILWINDCSS'],
+    codeRepo:"https://github.com/Rgit915/base-apparel-coming-soon-page",
+    liveLink:"https://base-apparel-coming-soon-rora.netlify.app/",
   },
   {
-    id: '004',
+    id: '006',
     image: taskImage6,
-    title: '',
+    title: 'Sunnyside agency landing page',
     description: [''],
-    codeRepo:"",
-    liveLink:"",
+    tools: ['REACT', 'TAILWINDCSS'],
+    codeRepo:"https://github.com/Rgit915/sunnyside-agency-landing-page",
+    liveLink:"https://sunnyside-agency-landing-page-rora.netlify.app/",
+  },
+  {
+    id: '007',
+    image: taskImage7,
+    title: 'Coding Bootcamp testimonals slider',
+    description: [''],
+    tools: ['REACT', 'TAILWINDCSS'],
+    codeRepo:"https://github.com/Rgit915/coding-bootcamp-testimonals-slider",
+    liveLink:"https://bootcamp-testimonal-slider-rora.netlify.app/",
+  },
+  {
+    id: '008',
+    image: taskImage8,
+    title: 'Blog preview card',
+    description: [''],
+    tools: ['HTML5','TAILWINDCSS'],
+    codeRepo:"https://github.com/Rgit915/blog-preview-card",
+    liveLink:"https://rgit915.github.io/blog-preview-card/",
   },
 ];
 
